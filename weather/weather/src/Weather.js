b73aafa68535d7ae97ab0a92a99eaf8b2e1e65d1
tejasri12345 <tejasri@applines.com>
2024-02-05 10:30:32 -0600

    minor changes

--- weather/weather/src/Weather.js
+++ weather/weather/src/Weather.js
@@ -9,54 +9,56 @@ function Weather() {
     e.preventDefault();
     // console.log("input city", e);
     const postUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputStr}&appid=${API_KEY}`;
-    axios.get(postUrl).then((response) => {
-      console.log("response", response);
-      if (response.data && response.data.list) {
-        //group the data by date
-        const groupedData = groupDataByDate(response.data.list);
-        setWeatherData(groupedData);
-      } else {
-        setWeatherData([]);
-      }
-    }).catch((error)=>{
-        window.alert("Something is invalid can you check again")
-    });
+    axios
+      .get(postUrl)
+      .then((response) => {
+        console.log("response", response);
+        if (response.data && response.data.list) {
+          //group the data by date
+          const groupedData = groupDataByDate(response.data.list);
+          setWeatherData(groupedData);
+        } else {
+          setWeatherData([]);
+        }
+      })
+      .catch((error) => {
+        window.alert("Something is invalid can you check again");
+      });
   };
   //it is to group weather data by date and calculate average temperature
   const groupDataByDate = (data) => {
     const groupedData = {};
     const currentDate = new Date().toLocaleDateString();
     data.forEach((item) => {
-        const date = new Date(item.dt * 1000).toLocaleDateString();
-        //it is for excluding current date
-        if (date!==currentDate) { 
-          if (!groupedData[date]) {
-            groupedData[date] = {
-              temperature:0,
-              pressure:0,
-              humidity:0,
-              count:0,
-            };
-          }
-          groupedData[date].temperature+=item.main.temp;
-          groupedData[date].pressure+=item.main.pressure;
-          groupedData[date].humidity+=item.main.humidity;
-          groupedData[date].count++;
+      const date = new Date(item.dt * 1000).toLocaleDateString();
+      //it is for excluding current date
+      if (date !== currentDate) {
+        if (!groupedData[date]) {
+          groupedData[date] = {
+            temperature: 0,
+            pressure: 0,
+            humidity: 0,
+            count: 0,
+          };
         }
-      });
-      console.log("goruuu",groupedData)
+        groupedData[date].temperature += item.main.temp;
+        groupedData[date].pressure += item.main.pressure;
+        groupedData[date].humidity += item.main.humidity;
+        groupedData[date].count++;
+      }
+    });
+    console.log("goruuu", groupedData);
     //calculating average temperature for each day
-    // Object.keys(groupedData).forEach((date) => {
-    //   groupedData[date].temperature /= groupedData[date].count;
-    //   groupedData[date].pressure /= groupedData[date].count;
-    //   groupedData[date].humidity /= groupedData[date].count;
-    // });
+    Object.keys(groupedData).forEach((date) => {
+      groupedData[date].temperature /= groupedData[date].count;
+      groupedData[date].pressure /= groupedData[date].count;
+      groupedData[date].humidity /= groupedData[date].count;
+    });
 
     return groupedData;
   };
   return (
     <div>
-      <div className="">
         <div className="cont-header">
           <h1>Weather in your City</h1>
         </div>
@@ -78,20 +80,20 @@ function Weather() {
           <div>
             {Object.keys(weatherData).map((date, index) => (
               <div key={index} className="table-container">
-                <h2> Date: {date}</h2>
+                <h2>Date: {date}</h2>
                 <table>
-                  <thead>
-                    <tr>
-                      <th>Temperature</th>
-                      <th>Pressure</th>
-                      <th>Humidity</th>
-                    </tr>
-                  </thead>
                   <tbody>
                     <tr>
-                      <td>{weatherData[date].temperature.toFixed(2)}</td>
-                      <td>{weatherData[date].pressure.toFixed(2)}</td>
-                      <td>{weatherData[date].humidity.toFixed(2)}</td>
+                      <th>Temperature</th>
+                      <td>{Math.round(weatherData[date].temperature)}</td>
+                    </tr>
+                    <tr>
+                      <th>Pressure</th>
+                      <td>{Math.round(weatherData[date].pressure)}</td>
+                    </tr>
+                    <tr>
+                      <th>Humidity</th>
+                      <td>{Math.round(weatherData[date].humidity)}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -100,7 +102,6 @@ function Weather() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
 export default Weather;
